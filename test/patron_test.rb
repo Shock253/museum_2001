68dd97b2e82315a31c25979bfae9d0494b08634a
Ethan Hocking <eshocking253@gmail.com>
2020-02-19 09:33:21 -0700

    Add add_interest method to Patron, corrected spelling errors

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -15,14 +15,14 @@ class PatronTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Bob", @patron_1.name
     assert_equal 20, @patron_1.spending_money
-    assert_equal [], @patron_1.intrests
+    assert_equal [], @patron_1.interests
   end
 
   def test_can_add_intrests
     @patron_1.add_interest("Dead Sea Scrolls")
     @patron_1.add_interest("Gems and Minerals")
 
-    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests 
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], @patron_1.interests
   end
 end
 
