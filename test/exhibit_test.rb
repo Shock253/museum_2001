fcf2af10e28e856ad355e0ff2e2e9e47f71844dc
Ethan Hocking <eshocking253@gmail.com>
2020-02-19 09:14:23 -0700

    Add test to Exhibit class

--- test/exhibit_test.rb
+++ test/exhibit_test.rb
@@ -12,15 +12,17 @@ class ExhibitTest < Minitest::Test
     assert_instance_of Exhibit, @exhibit
   end
 
+  def test_it_has_attributes
+    assert_equal "Gems and Minerals", @exhibit.name
+    assert_equal 0, @exhibit.cost
+  end
+  
 end
 
 
 # pry(main)> require './lib/exhibit'
 # # => true
-#
-# pry(main)> require './lib/patron'
-# # => true
-#
+
 # pry(main)> exhibit = Exhibit.new({name: "Gems and Minerals", cost: 0})
 # # => #<Exhibit:0x00007fcb13bd22d0...>
 #
@@ -29,22 +31,3 @@ end
 #
 # pry(main)> exhibit.cost
 # # => 0
-#
-# pry(main)> patron_1 = Patron.new("Bob", 20)
-# # => #<Patron:0x00007fcb13b5c7d8...>
-#
-# pry(main)> patron_1.name
-# # => "Bob"
-#
-# pry(main)> patron_1.spending_money
-# # => 20
-#
-# pry(main)> patron_1.interests
-# # => []
-#
-# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
-#
-# pry(main)> patron_1.add_interest("Gems and Minerals")
-#
-# pry(main)> patron_1.interests
-# # => ["Dead Sea Scrolls", "Gems and Minerals"]
