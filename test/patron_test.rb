0a0d90b76091d7184972c28b0c145268f9ed49de
Ethan Hocking <eshocking253@gmail.com>
2020-02-19 09:20:46 -0700

    Add test file for Patron class

---
+++ test/patron_test.rb
@@ -0,0 +1,36 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/patron"
+
+class PatronTest < Minitest::Test
+
+  def setup
+    @patron_1 = Patron.new("Bob", 20)
+  end
+
+  def test_it_exists
+    assert_instance_of Patron, @patron_1
+  end
+end
+
+
+
+#
+# pry(main)> patron_1 = Patron.new("Bob", 20)
+# # => #<Patron:0x00007fcb13b5c7d8...>
+#
+# pry(main)> patron_1.name
+# # => "Bob"
+#
+# pry(main)> patron_1.spending_money
+# # => 20
+#
+# pry(main)> patron_1.interests
+# # => []
+#
+# pry(main)> patron_1.add_interest("Dead Sea Scrolls")
+#
+# pry(main)> patron_1.add_interest("Gems and Minerals")
+#
+# pry(main)> patron_1.interests
+# # => ["Dead Sea Scrolls", "Gems and Minerals"]
