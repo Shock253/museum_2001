37f4016a605feb29ab35c721ac76e64b044ce352
Ethan Hocking <eshocking253@gmail.com>
2020-02-19 09:29:58 -0700

    Add test for adding intrests to Patron class

--- test/patron_test.rb
+++ test/patron_test.rb
@@ -15,25 +15,19 @@ class PatronTest < Minitest::Test
   def test_it_has_attributes
     assert_equal "Bob", @patron_1.name
     assert_equal 20, @patron_1.spending_money
-    assert_equal [], @patron_1.intrests 
+    assert_equal [], @patron_1.intrests
+  end
+
+  def test_can_add_intrests
+    @patron_1.add_interest("Dead Sea Scrolls")
+    @patron_1.add_interest("Gems and Minerals")
+
+    assert_equal ["Dead Sea Scrolls", "Gems and Minerals"], patron_1.interests 
   end
 end
 
 
 
-#
-# pry(main)> patron_1 = Patron.new("Bob", 20)
-# # => #<Patron:0x00007fcb13b5c7d8...>
-#
-# pry(main)> patron_1.name
-# # => "Bob"
-#
-# pry(main)> patron_1.spending_money
-# # => 20
-#
-# pry(main)> patron_1.interests
-# # => []
-#
 # pry(main)> patron_1.add_interest("Dead Sea Scrolls")
 #
 # pry(main)> patron_1.add_interest("Gems and Minerals")
